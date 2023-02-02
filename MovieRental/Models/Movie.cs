8abf2b323ccbe7f53703b3da107c6cf626dd23b5
AnxhelaAsimi <87004166+AnxhelaAsimi@users.noreply.github.com>
2023-02-02 21:54:43 +0100

    Adding Models

---
+++ MovieRental/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿using MovieRental.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRental.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public string  ImageURL { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+        //Store
+        public int StoreId { get; set; }
+
+        [ForeignKey("StoreId")]
+        public PhysicalStore Store { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+
+        [ForeignKey("ProducerId")]
+        public Producer Producer { get; set; }
+    }
+}
---
+++ MovieRental/Models/PhysicalStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRental.Models
+{
+    public class PhysicalStore
+    {
+        [Key]
+        public int Id { get; set; }
+        public string logoURL { get; set; }
+        public string name { get; set; }
+        public string Address { get; set; }
+
+        //Relationships
+        public  List<Movie> Movies { get; set; }
+    }
+}
